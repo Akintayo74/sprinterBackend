3a288ab5059fbed1466e2b6a18fc10cbf48ed21d
GooseGustin <pammafeng11@gmail.com>
2025-08-27 16:03:02 +0100

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  // get config service after app is created
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
+
+  app.enableCors({
+    origin: [frontendUrl, 'https://your-frontend.com'], // allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allow cookies/auth headers
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
